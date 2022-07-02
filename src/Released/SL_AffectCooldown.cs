3956982cb7bb04b659262b84bd274b7be5fedef2
Dsze <danzap95@gmail.com>
2022-07-02 14:24:34 -0400

    Whitelist skills, fix ParentEffect QoL

--- src/Released/SL_AffectCooldown.cs
+++ src/Released/SL_AffectCooldown.cs
@@ -14,12 +14,14 @@ namespace SideLoader_ExtendedEffects
 
         public float Amount;
         public bool IsModifier;
+        public int[] AllowedSkills;
 
         public override void ApplyToComponent<T>(T component)
         {
             AffectCooldown effect = component as AffectCooldown;
             effect.Amount = this.Amount;
             effect.IsModifier = this.IsModifier;
+            effect.AllowedSkills = this.AllowedSkills;
         }
 
         public override void SerializeEffect<T>(T effect)
@@ -27,6 +29,7 @@ namespace SideLoader_ExtendedEffects
             AffectCooldown comp = effect as AffectCooldown;
             this.Amount = comp.Amount;
             this.IsModifier = comp.IsModifier;
+            this.AllowedSkills = comp.AllowedSkills;
         }
     }
 
@@ -36,6 +39,7 @@ namespace SideLoader_ExtendedEffects
 
         public float Amount;
         public bool IsModifier;
+        public int[] AllowedSkills;
 
         public override void ActivateLocally(Character _affectedCharacter, object[] _infos)
         {
@@ -43,11 +47,16 @@ namespace SideLoader_ExtendedEffects
             foreach (string uid in skills)
             {
                 Skill skill = ItemManager.Instance.GetItem(uid) as Skill;
+                SL.Log(skill.Name + " " + skill.ItemID);
+                if (AllowedSkills != null && AllowedSkills.Length > 0 && !AllowedSkills.Contains(skill.ItemID))
+                {
+                    SL.Log("Not Whitelisted");
+                    continue; // Not an allowed skill
+                }
+
                 float amount = IsModifier ? skill.RealCooldown * Amount/100 : Amount;
-                SL.Log("Cooldown on " + skill.name + " reduced by " + Amount);
                 skill.m_remainingCooldownTime -= amount;
                 skill.UpdateCooldownRatio();
-                SL.Log("Cooldown on " + skill.name + " now " + skill.m_remainingCooldownTime);
             }
         }
     }
